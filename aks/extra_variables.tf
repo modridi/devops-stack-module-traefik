054ac1d75810a2fa30ea4556e0702855c69d19d3
Federico Sismondi <federico.sismondi@camptocamp.com>
2022-10-20 16:46:53 +0200

    doc: update var description

--- aks/extra_variables.tf
+++ aks/extra_variables.tf
@@ -1,4 +1,4 @@
 variable "resource_group_name" {
-  description = "The Resource Group where the Managed Kubernetes Cluster should exist."
+  description = "The Resource Group where the DNS Zone exists. DNS Zone is updated with CNAME *.apps.cluster_name -> load balancer's FQDNs. This RG and AKS's RG must be in the same location."
   type        = string
 }
