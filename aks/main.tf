f887b7f781a0e9345c069bda9bac50a939f77a3e
Xavier Rakotomamonjy <Rakotomamonjy.xavier@gmail.com>
2022-12-06 13:42:10 +0100

    Fix/propagate target revision (#23)
    * fix(target_revision): propagate attribut
    
    * fix(target_revision): propagate attribut in aks, eks, sks.
    
    * docs(terraform-docs): generate docs and write to README.adoc
    
    Co-authored-by: Xavier Rakotomamonjy <xavier.rakotomamonjy@camptocamp.com>
    Co-authored-by: Xaving <Xaving@users.noreply.github.com>
    
    Release-As: v1.0.0-alpha.4

--- aks/main.tf
+++ aks/main.tf
@@ -23,6 +23,7 @@ module "traefik" {
   base_domain      = var.base_domain
   argocd_namespace = var.argocd_namespace
 
+  target_revision = var.target_revision
   namespace = var.namespace
 
   helm_values = concat(local.helm_values, var.helm_values)
